db96b3de8b8d0f0ede32b78b8c51388e8655bb3a
monahvi <nahvi.moghaddam@gmail.com>
2023-08-12 13:29:57 +0100

    frist commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const searchImages = async (term) => {
+   const response =  await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID rMPFKK8U7lR10h_DZY8eWxmWuXBOoQxTkZKE8bUjW6Q'
+        },
+        params: {
+            query: term,
+        }
+    });
+
+    return response.data.results;
+};
+
+export default searchImages;
